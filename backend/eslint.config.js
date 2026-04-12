dd876d26de71ad2f30c15798bbdc96a2f96d8c5a
Samuel Townsend <stownsend@vertafore.com>
2026-04-11 21:51:58 -0400

    eslint and prettier configuration

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,7 +1,4 @@
 import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -10,16 +7,9 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-      prettier,
-    ],
+    extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
